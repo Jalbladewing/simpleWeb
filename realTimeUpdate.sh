ab0f399a5f29d7ea88ef6059b208d6d7b92d064c
Juan Alberto Llopis <juanalbertollopis@realtracksystems.com>
2019-12-24 14:40:30 +0100

    Añadido script para actualización de datos en tiempo real.

---
+++ realTimeUpdate.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+failures=0
+number=0
+
+# never exit
+while (("$failures" < 9999));
+do
+	number=$((20 + RANDOM%(1+27-20)))
+	curl -iX PUT --url 'http://192.168.111.129:1026/v2/entities/Room1/attrs/temperature/value'   --header 'Content-Type: text/plain'   --data $number
+	sleep 1
+done
